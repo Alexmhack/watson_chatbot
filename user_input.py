8e04efe053a26bff6f604619f51925161b1e6f44
alexHooper <alexmohack@gmail.com>
2018-09-13 11:39:01 +0530

    run conversation in while loop

--- user_input.py
+++ user_input.py
@@ -12,3 +12,23 @@ service = watson_developer_cloud.AssistantV1(
 )
 
 workspace_id = os.getenv("WATSON_WORKSPACEID")
+
+# initialize with empty value to start conversation
+user_message = ''
+
+while True:
+
+	response = service.message(
+		workspace_id=workspace_id,
+		input={
+			'text': user_message
+		}
+	).get_result()
+
+	if response['intents']:
+		print(f"Detected intent: {response['intents'][0]['intent']}")
+
+	if response['output']['text']:
+		print(response['output']['text'][0])
+
+	user_message = input(">> ")
