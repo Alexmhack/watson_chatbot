3cf14dbf08145dfbb7fd805a66440cbaf8480840
alexHooper <alexmohack@gmail.com>
2018-09-13 09:45:15 +0530

    use env variables for watson credentails

--- client_app.py
+++ client_app.py
@@ -5,23 +5,18 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-# service = watson_developer_cloud.AssistantV1(
-# 	username=os.getenv("USERNAME"),
-# 	password=os.getenv("PASSWORD"),
-# 	version=os.getenv("VERSION")
-# )
+service = watson_developer_cloud.AssistantV1(
+	username=os.getenv("WATSON_USERNAME"),
+	password=os.getenv("WATSON_PASSWORD"),
+	version=os.getenv("WATSON_VERSION")
+)
 
-# workspace_id = os.getenv("WORKSPACEID")
+workspace_id = os.getenv("WATSON_WORKSPACEID")
 
-# # Start conversation with an empty message
-# response = service.message(
-# 	workspace_id=workspace_id,
-# 	input={
-# 		'text': ''
-# 	}
-# )
-
-print(os.getenv("WATSON_VERSION"))
-print(os.getenv("WATSON_USERNAME"))
-print(os.getenv("WATSON_WORKSPACEID"))
-print(os.getenv("WATSON_PASSWORD"))
+# Start conversation with an empty message
+response = service.message(
+	workspace_id=workspace_id,
+	input={
+		'text': ''
+	}
+)
